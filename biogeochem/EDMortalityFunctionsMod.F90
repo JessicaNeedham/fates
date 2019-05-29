fee9bd4164837ec9d7639270fc3987c728725506
JessicaNeedham <jfneedham@lbl.gov>
2019-05-29 14:46:21 -0700

    Fixed a bug that confused daily and annual logging rates in the daily mortality cap - specific to senescence

--- biogeochem/EDMortalityFunctionsMod.F90
+++ biogeochem/EDMortalityFunctionsMod.F90
@@ -222,9 +222,10 @@ if (hlm_use_ed_prescribed_phys .eq. ifalse) then
                        currentCohort%lmort_infra)/hlm_freq_day
 
        ! this check caps smort so that daily mortality cannot exceed 0.995
-       if (((cmort+hmort+bmort+frmort+smort)/hlm_freq_day + dndt_logging) &
+       if ((cmort+hmort+bmort+frmort+smort + dndt_logging)*hlm_freq_day &
             > 0.995_r8)then
-          smort = 0.995_r8 - ((cmort+hmort+bmort+frmort)/hlm_freq_day + dndt_logging)
+          smort = (0.995_r8 - ((cmort+hmort+bmort+frmort+dndt_logging)*hlm_freq_day)) / &
+               hlm_freq_day
        endif
 
        currentCohort%dndt = -1.0_r8 * (cmort+hmort+bmort+frmort+smort + dndt_logging) &
@@ -233,12 +234,13 @@ if (hlm_use_ed_prescribed_phys .eq. ifalse) then
 
        ! cap on smort for canopy layers - smort is adjusted so that total mortality
        ! including disturbance fraction does not exceed 0.995
-       if(((cmort+hmort+bmort+frmort+smort)/hlm_freq_day) > & 
+       if(((cmort+hmort+bmort+frmort+smort)*hlm_freq_day) > & 
             0.995_r8 - fates_mortality_disturbance_fraction)then
-          smort=0.995_r8 - fates_mortality_disturbance_fraction - & 
-               ((cmort+hmort+bmort+frmort)/hlm_freq_day)
+          smort= (0.995_r8 - fates_mortality_disturbance_fraction - & 
+               ((cmort+hmort+bmort+frmort)*hlm_freq_day)/hlm_freq_day
        endif
-       currentCohort%dndt = -1.0_r8 * (cmort+hmort+bmort+frmort+dndt_logging) * currentCohort%n
+       currentCohort%dndt = -(1.0_r8 - fates_mortality_disturbance_fraction) &
+            * (cmort+hmort+bmort+frmort+dndt_logging) * currentCohort%n
    
        endif
 
