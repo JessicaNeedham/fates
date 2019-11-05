ff992a158514d9ef9cc513f5677d4e81f3277d8b
Jessica Needham <jfneedham@n0002.scs00>
2019-11-04 17:50:09 -0800

    Python script to make 1 pft ensemble runs asmort.

---
+++ parameter_files/Param_generator_asmort_1pfts.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3                                                                                                                                                                      
+# -*- coding: utf-8 -*-                                                                                                                                                                     
+"""                                                                                                                                                                                         
+Created on Wed Sep 11 16:08:44 2019                                                                                                                                                         
+                                                                                                                                                                                            
+@author: JFNeedham                                                                                                                                                                          
+"""
+import numpy as np
+#import matplotlib.pyplot as plt                                                                                                                                                            
+import modp
+import os
+from scipy.io import netcdf as nc
+import argparse
+import shutil
+import tempfile
+import sys
+import datetime
+import time
+
+
+###                                                                                                                                                                                         
+n = 1
+
+canopy_mort = np.linspace(0.005, 0.05, num=10)
+ip = np.linspace(80,260, num=10)
+print(canopy_mort)
+print(ip)
+
+
+for i in range(0,10) :
+    
+    for j in range(0,10) :
+
+        fileout = os.path.join('onepft_ensembles', 'fates_params_asmort_1pft_%d.nc'  % (n))                                                                                                                                                          
+        pft = 1
+        fin = fileout
+
+
+        # The first call to main we generate a new file                                                                                                                                         
+        var = 'fates_prescribed_mortality_canopy'
+        modp.main(var = var, pft = pft, fin = 'onepft_ensembles/fates_params_asmort_1pft_1.nc',
+              val = canopy_mort[i],  fout = fileout, O = 0)
+   
+        var = 'fates_mort_ip_senescence'
+        modp.main(var = var, pft = pft, fin = fin,
+              val = canopy_mort[j], fout = fileout, O = 1)
+    
+        n = n+1
+        print(n)
+
+     
