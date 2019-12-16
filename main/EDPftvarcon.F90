1237c84692db0db463fdc72d2da0d0e72db14021
Jessica Needham <jfneedham@n0001.scs00>
2019-12-16 14:45:11 -0800

    [ Insert max dbh parameter into EDPftvarcon ]
    [ Max dbh parameter into EDPftvarcon ]
    
    Fixes: [NGT-ED Github issue #]
    
    User interface changes?: [ No]
    
    Code review: [Names]
    
    Test suite: [suite name, machine, compilers]
    Test baseline:
    Test namelist changes:
    Test answer changes: [bit for bit, roundoff, climate changing]
    Test summary: No test

--- main/EDPftvarcon.F90
+++ main/EDPftvarcon.F90
@@ -94,6 +94,7 @@ module EDPftvarcon
      real(r8), allocatable :: maintresp_reduction_intercept(:) ! intercept of MR reduction as f(carbon storage), 
                                                                ! 0=no throttling, 1=max throttling
      real(r8), allocatable :: bmort(:)
+     real(r8), allocatable :: max_dbh(:)                       ! hard cap on dbh - for prescribed phys mode
      real(r8), allocatable :: mort_scalar_coldstress(:)
      real(r8), allocatable :: mort_scalar_cstarvation(:)
      real(r8), allocatable :: mort_scalar_hydrfailure(:)
@@ -662,6 +663,10 @@ contains
     call fates_params%RegisterParameter(name=name, dimension_shape=dimension_shape_1d, &
          dimension_names=dim_names, lower_bounds=dim_lower_bound)
 
+    name = 'fates_max_dbh' 
+    call fates_params%RegisterParameter(name=name, dimension_shape=dimension_shape_1d, &
+         dimension_names=dim_names, lower_bounds=dim_lower_bound)
+
     name = 'fates_mort_scalar_coldstress'
     call fates_params%RegisterParameter(name=name, dimension_shape=dimension_shape_1d, &
          dimension_names=dim_names, lower_bounds=dim_lower_bound)
@@ -1092,6 +1097,10 @@ contains
     name = 'fates_mort_bmort'
     call fates_params%RetreiveParameterAllocate(name=name, &
          data=this%bmort)
+
+    name = 'fates_max_dbh'
+    call fates_params%RetreiveParameterAllocate(name=name, &
+         data=this%max_dbh)
 
     name = 'fates_mort_scalar_coldstress'
     call fates_params%RetreiveParameterAllocate(name=name, &
@@ -1753,6 +1762,7 @@ contains
         write(fates_log(),fmt0) 'grperc = ',EDPftvarcon_inst%grperc
         write(fates_log(),fmt0) 'c2b = ',EDPftvarcon_inst%c2b
         write(fates_log(),fmt0) 'bmort = ',EDPftvarcon_inst%bmort
+        write(fates_log(),fmt0) 'max_dbh = ',EDPftvarcon_inst%max_dbh
         write(fates_log(),fmt0) 'mort_scalar_coldstress = ',EDPftvarcon_inst%mort_scalar_coldstress
         write(fates_log(),fmt0) 'mort_scalar_cstarvation = ',EDPftvarcon_inst%mort_scalar_cstarvation
         write(fates_log(),fmt0) 'mort_scalar_hydrfailure = ',EDPftvarcon_inst%mort_scalar_hydrfailure
